a5321ee0723a48dc25ffdd5105daf1796054999f
FadingWinds <wq.tang26@gmail.com>
2020-03-27 22:39:00 -0400

    update alignment

--- alignment-ref.py
+++ alignment-ref.py
@@ -42,22 +42,26 @@ def computeHomography(f1, f2, matches, A_out=None):
         #Fill in the matrix A in this loop.
         #Access elements using square brackets. e.g. A[0,0]
         #TODO-BLOCK-BEGIN
-        A[2*i, 0] = a_x
-        A[2*i, 1] = a_y
-        A[2*i, 2] = 1
+        for i in range(num_matches):
+            m = matches[i]
+            x1, y1 = f1[m.queryIdx].pt
+            x2, y2 = f2[m.trainIdx].pt
+            
+            A[2*i,0] = x1
+            A[2*i,1] = y1
+            A[2*i,2] = 1
 
-        A[2*i, 6] = -b_x * a_x
-        A[2*i, 7] = -b_x * a_y
-        A[2*i, 8] = -b_x
-
-        A[2*i+1, 3] = a_x
-        A[2*i+1, 4] = a_y
-        A[2*i+1, 5] = 1
-
-        A[2*i+1, 6] = -b_y * a_x
-        A[2*i+1, 7] = -b_y * a_y
-        A[2*i+1, 8] = -b_y
-        # TODO 2 Implemented
+            A[2*i,6] = -x2*x1
+            A[2*i,7] = -x2*y1
+            A[2*i,8] = -x2
+            
+            A[2*i+1,3] = x1
+            A[2*i+1,4] = y1
+            A[2*i+1,5] = 1
+            
+            A[2*i+1,6] = -y2*x1
+            A[2*i+1,7] = -y2*y1
+            A[2*i+1,8] = -y2
         #TODO-BLOCK-END
         #END TODO
 
@@ -77,8 +81,11 @@ def computeHomography(f1, f2, matches, A_out=None):
     #BEGIN TODO 3
     #Fill the homography H with the appropriate elements of the SVD
     #TODO-BLOCK-BEGIN
-    H = Vt[-1].reshape((3,3))
-    # TODO 3 Implemented (not sure)
+    num_eig = Vt.shape[0]
+    h = Vt[num_eig-1, :]
+    for i in range(3):
+        for j in range(3):
+            H[i,j] = h[3*i + j]
     #TODO-BLOCK-END
     #END TODO
 
@@ -96,7 +103,6 @@ def alignPair(f1, f2, matches, m, nRANSAC, RANSACthresh):
         m -- MotionModel (eTranslate, eHomography)
         nRANSAC -- number of RANSAC iterations
         RANSACthresh -- RANSAC distance threshold
-
     Output:
         M -- inter-image transformation matrix
         Repeat for nRANSAC iterations:
@@ -110,7 +116,7 @@ def alignPair(f1, f2, matches, m, nRANSAC, RANSACthresh):
 
     #BEGIN TODO 4
     #Write this entire method.  You need to handle two types of
-    #motion models, pure translations (m == eTranslation) and
+    #motion models, pure translations (m == eTranslate) and
     #full homographies (m == eHomography).  However, you should
     #only have one outer loop to perform the RANSAC code, as
     #the use of RANSAC is almost identical for both cases.
@@ -120,27 +126,32 @@ def alignPair(f1, f2, matches, m, nRANSAC, RANSACthresh):
     #least_squares_fit.
     #TODO-BLOCK-BEGIN
     if m == eTranslate:
-        sample = 1
+        s = 1
     elif m == eHomography:
-        sample = 4
-    max_inlier = 0
-    max_est = []
+        s = 4
+    
+    num_inliers = -1
+    best_estimate = []    
     for i in range(nRANSAC):
-        H = np.eye(3)
-        randMatch = np.random.choice(matches, sample)
-        if m == eHomography:
-            H = computeHomography(f1,f2,randMatch)
-        elif m == eTranslate:
-            x1, y1 = f1[randMatch[0].queryIdx].pt
-            x2, y2 = f2[randMatch[0].trainIdx].pt
+        matches_sub = np.random.choice(matches, s, replace=False)
+        if m == 1:
+            H = computeHomography(f1, f2, matches_sub)
+        else:
+            x1, y1 = f1[matches_sub[0].queryIdx].pt
+            x2, y2 = f2[matches_sub[0].trainIdx].pt
+            H = np.zeros((3,3))
+            H[0,0] = 1
+            H[1,1] = 1
+            H[2,2] = 1
             H[0,2] = x2 - x1
             H[1,2] = y2 - y1
-        num_inliers = getInliers(f1, f2, matches, H, RANSACthresh)
-        if len(num_inliers) > max_inlier:
-            max_est = num_inliers
-            max_inlier = len(num_inliers)
-    M = leastSquaresFit(f1, f2, matches, m, max_est)
-    # TODO 4 implemented
+        inlier_indices = getInliers(f1, f2, matches, H, RANSACthresh)
+        if (len(inlier_indices) > num_inliers):
+            num_inliers = len(inlier_indices)
+            best_estimate = inlier_indices
+    
+    M = leastSquaresFit(f1, f2, matches, m, best_estimate)
+        
     #TODO-BLOCK-END
     #END TODO
     return M
@@ -156,10 +167,8 @@ def getInliers(f1, f2, matches, M, RANSACthresh):
             DMatch.distance: The distance between the two features
         M -- inter-image transformation matrix
         RANSACthresh -- RANSAC distance threshold
-
     Output:
         inlier_indices -- inlier match indices (indexes into 'matches')
-
         Transform the matched features in f1 by M.
         Store the match index of features in f1 for which the transformed
         feature is within Euclidean distance RANSACthresh of its match
@@ -177,19 +186,19 @@ def getInliers(f1, f2, matches, M, RANSACthresh):
         #TODO-BLOCK-BEGIN
         x1, y1 = f1[matches[i].queryIdx].pt
         x2, y2 = f2[matches[i].trainIdx].pt
-        p = np.zeros((3,1))
-        p[0] = x1
-        p[1] = y1
-        p[2] = 1
-        trans = np.dot(M,p)
-        xt = trans[0]/trans[2]
-        yt = trans[1]/trans[2]
-        x_dist = (x2 - xt)**2
-        y_dist = (y2 - xt)**2
-        dist = np.sqrt(x_dist+y_dist)
-        if dist <= RANSACthresh:
+        
+        x = np.zeros((3,1))
+        x[0] = x1
+        x[1] = y1
+        x[2] = 1
+        y = np.dot(M,x)
+        
+        x2_pred = y[0] / y[2]
+        y2_pred = y[1] / y[2]
+        
+        dist = np.sqrt((x2-x2_pred)**2 + (y2 - y2_pred)**2)
+        if (dist <= RANSACthresh):
             inlier_indices.append(i)
-        # TODO 5 implemented
         #TODO-BLOCK-END
         #END TODO
 
@@ -206,10 +215,8 @@ def leastSquaresFit(f1, f2, matches, m, inlier_indices):
             DMatch.distance: The distance between the two features
         m -- MotionModel (eTranslate, eHomography)
         inlier_indices -- inlier match indices (indexes into 'matches')
-
     Output:
         M - transformation matrix
-
         Compute the transformation matrix from f1 to f2 using only the
         inliers and return it.
     '''
@@ -236,9 +243,9 @@ def leastSquaresFit(f1, f2, matches, m, inlier_indices):
             #TODO-BLOCK-BEGIN
             x1, y1 = f1[matches[inlier_indices[i]].queryIdx].pt
             x2, y2 = f2[matches[inlier_indices[i]].trainIdx].pt
+            
             u += x2 - x1
             v += y2 - y1
-            # TODO 6 implemented
             #TODO-BLOCK-END
             #END TODO
 
@@ -255,9 +262,9 @@ def leastSquaresFit(f1, f2, matches, m, inlier_indices):
         #TODO-BLOCK-BEGIN
         best_matches = []
         for i in range(len(inlier_indices)):
-            best_matches.append(matches[inlier_indices[i]])        
+            best_matches.append(matches[inlier_indices[i]])
+        
         M = computeHomography(f1, f2, best_matches)
-        # TODO 7 implemented
         #TODO-BLOCK-END
         #END TODO
 
@@ -265,4 +272,3 @@ def leastSquaresFit(f1, f2, matches, m, inlier_indices):
         raise Exception("Error: Invalid motion model.")
 
     return M
-
